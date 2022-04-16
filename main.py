3a8c664d7e0793e4a8b7d2badd2b3f085643dfe5
Sus4no <96766854+Sus4no@users.noreply.github.com>
2022-04-16 14:27:17 +0300

    added base.html

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
-users = ['user1', 'user2', 'etc']
+users = ['user1', 'user2', 'user3', 'user4', 'user5']
+messages = ['Lorem ipsum dolor', '2', 'Some txt', '4', 'etc']
 
 
-# @app.route('/')
-# def welcome_page():
-#     return render_template('welcome.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 @app.route('/contacts')
-def main_page():
-    return render_template('main.html', users=users)
+def contacts_page():
+    return render_template('contacts.html', users=users)
+
+
+@app.route('/contacts/<user_id>')
+def dialogue_page(user_id):
+    return render_template('conversation.html', messages=messages)
 
 
 def main():
